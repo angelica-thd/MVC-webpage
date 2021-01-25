acc6e055397af2b8b0ddb185b968f23eb6f5d068
angelica-thd <ageliki.th@gmail.com>
2021-01-25 18:17:51 +0200

    first commit

---
+++ WebApplication2/Controllers/SalesController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class SalesController : Controller
+    {
+        private pubsEntities db = new pubsEntities();
+
+        // GET: Sales
+        public ActionResult Index()
+        {
+            var sales = db.sales.Include(s => s.store).Include(s => s.title);
+            return View(sales.ToList());
+        }
+
+        // GET: Sales/Details/5
+        public ActionResult Details(string stor_id, string ord_num, string title_id)
+        {
+            if (stor_id == null || ord_num == null || title_id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            sale sale = db.sales.Find(stor_id,ord_num,title_id);
+            if (sale == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sale);
+        }
+
+        // GET: Sales/Create
+        public ActionResult Create()
+        {
+            ViewBag.stor_id = new SelectList(db.stores, "stor_id", "stor_name");
+            ViewBag.title_id = new SelectList(db.titles, "title_id", "title1");
+            return View();
+        }
+
+        // POST: Sales/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "stor_id,ord_num,ord_date,qty,payterms,title_id")] sale sale)
+        {
+            if (ModelState.IsValid)
+            {
+                db.sales.Add(sale);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.stor_id = new SelectList(db.stores, "stor_id", "stor_name", sale.stor_id);
+            ViewBag.title_id = new SelectList(db.titles, "title_id", "title1", sale.title_id);
+            return View(sale);
+        }
+
+        // GET: Sales/Edit/5
+        public ActionResult Edit(string stor_id, string ord_num, string title_id)
+        {
+            if (stor_id == null || ord_num == null || title_id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            sale sale = db.sales.Find(stor_id, ord_num, title_id);
+            if (sale == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.stor_id = new SelectList(db.stores, "stor_id", "stor_name", sale.stor_id);
+            //ViewBag.ord_num = new SelectList(db.stores, "ord_num", "order_name", sale.ord_num);
+            ViewBag.title_id = new SelectList(db.titles, "title_id", "title1", sale.title_id);
+            return View(sale);
+        }
+
+        // POST: Sales/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "stor_id,ord_num,ord_date,qty,payterms,title_id")] sale sale)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sale).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.stor_id = new SelectList(db.stores, "stor_id", "stor_name", sale.stor_id);
+            ViewBag.title_id = new SelectList(db.titles, "title_id", "title1", sale.title_id);
+            return View(sale);
+        }
+
+        // GET: Sales/Delete/5
+        public ActionResult Delete(string stor_id, string ord_num, string title_id)
+        {
+            if (stor_id == null || ord_num == null || title_id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            sale sale = db.sales.Find(stor_id, ord_num, title_id);
+            if (sale == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sale);
+        }
+
+        // POST: Sales/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string stor_id, string ord_num, string title_id)
+        {
+            sale sale = db.sales.Find(stor_id, ord_num, title_id);
+            db.sales.Remove(sale);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication2/Controllers/TitleauthorsController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class TitleauthorsController : Controller
+    {
+        private pubsEntities db = new pubsEntities();
+
+        // GET: Titleauthors
+        public ActionResult Index()
+        {
+            var titleauthors = db.titleauthors.Include(t => t.author).Include(t => t.title);
+            return View(titleauthors.ToList());
+        }
+        
+        public String getauthorID(String id)
+        {
+            var authors = db.authors.Find(id);
+            return authors.ToString();
+        }
+
+        // GET: Titleauthors/Details/5
+        public ActionResult Details(string au_id,string title_id)
+        {
+            if (title_id == null || au_id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            titleauthor titleauthor = db.titleauthors.Find(au_id, title_id);
+
+            if (titleauthor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(titleauthor);
+        }
+
+        // GET: Titleauthors/Create
+        public ActionResult Create()
+        {
+            ViewBag.au_id = new SelectList(db.authors, "au_id", "au_lname");
+            ViewBag.title_id = new SelectList(db.titles, "title_id", "title1");
+            return View();
+        }
+
+        // POST: Titleauthors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "au_id,title_id,au_ord,royaltyper")] titleauthor titleauthor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.titleauthors.Add(titleauthor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.au_id = new SelectList(db.authors, "au_id", "au_lname", titleauthor.au_id);
+            ViewBag.title_id = new SelectList(db.titles, "title_id", "title1", titleauthor.title_id);
+            return View(titleauthor);
+        }
+
+        // GET: Titleauthors/Edit/5
+        public ActionResult Edit(string au_id, string title_id)
+        {
+            if (title_id == null || au_id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            titleauthor titleauthor = db.titleauthors.Find(au_id, title_id);
+            if (titleauthor == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.au_id = new SelectList(db.authors, "au_id", "au_lname", titleauthor.au_id);
+            ViewBag.title_id = new SelectList(db.titles, "title_id", "title1", titleauthor.title_id);
+            return View(titleauthor);
+        }
+
+        // POST: Titleauthors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "au_id,title_id,au_ord,royaltyper")] titleauthor titleauthor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(titleauthor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.au_id = new SelectList(db.authors, "au_id", "au_lname", titleauthor.au_id);
+            ViewBag.title_id = new SelectList(db.titles, "title_id", "title1", titleauthor.title_id);
+            return View(titleauthor);
+        }
+
+        // GET: Titleauthors/Delete/5
+        public ActionResult Delete(string au_id, string title_id)
+        {
+            if (title_id == null || au_id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            titleauthor titleauthor = db.titleauthors.Find(au_id, title_id);
+            if (titleauthor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(titleauthor);
+        }
+
+        // POST: Titleauthors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string au_id, string title_id)
+        {
+            titleauthor titleauthor = db.titleauthors.Find(au_id, title_id);
+            db.titleauthors.Remove(titleauthor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
